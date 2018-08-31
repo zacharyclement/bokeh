18aacf980d13e13b5a8fd748f01f57eaded01caa
zachclem42@gmail.com <ZCC@ZachsMachine.fios-router.home>
2018-08-31 13:48:15 -0400

    change slider and button figures

--- bokeh_dashboard/airports.py
+++ bokeh_dashboard/airports.py
@@ -32,10 +32,16 @@ del df['index']
 data = df[['year', 'locationid', 'costs', 'trainees_total', 'trainees_pass', 'latitude', 'longitude']]
 data = data.set_index('year')
 
-source = ColumnDataSource(dict(
+source1 = ColumnDataSource(dict(
     x = data.loc[2000].trainees_total,
-    y = data.loc[2000].costs,    
+    y = data.loc[2000].costs,
     location=data.loc[2000].locationid,
+))
+
+source2 = ColumnDataSource(dict(
+    x = data.trainees_total,
+    y = data.costs,
+    location=data.locationid,
 ))
 
 #map data
@@ -53,6 +59,7 @@ map_source = ColumnDataSource(
     )
 )
 
+######Figure 1
 
 PLOT_OPTS = dict(
     plot_height = 400,
@@ -60,78 +67,91 @@ PLOT_OPTS = dict(
     y_range = (data['costs'].min(), data['costs'].max()),
     )
 
-p = figure(
+p1 = figure(
     title='costs per trainee',
     tools=[HoverTool(tooltips='@location', show_arrow=False)],
     **PLOT_OPTS)
 
-p.circle(
+p1.circle(
     x='x',
     y='y',
-    source=source,
+    source=source1,
 )
 
-p.xaxis[0].formatter = NumeralTickFormatter()
-p.xaxis.axis_label = "# of Total Trainees"
-p.yaxis.axis_label = "Costs"
+p1.xaxis[0].formatter = NumeralTickFormatter()
+p1.xaxis.axis_label = "# of Total Trainees"
+p1.yaxis.axis_label = "Costs"
 
+###########Figure 2
 
-p.xaxis[0].formatter = NumeralTickFormatter()
-p.xaxis.axis_label = "# of Total Trainees"
-p.yaxis.axis_label = "Costs"
+p2 = figure(
+    title='costs per trainee',
+    #tools=[HoverTool(tooltips='@location', show_arrow=False)],
+    **PLOT_OPTS)
 
+p2.circle(
+    x='x',
+    y='y',
+    source=source2,
+)
+
+p2.xaxis[0].formatter = NumeralTickFormatter()
+p2.xaxis.axis_label = "# of Total Trainees"
+p2.yaxis.axis_label = "Costs"
+
+#############Figure 3 - map
 
 googleAPI= 'AIzaSyBaL6-f-crVqoA88nnRsF0FtDhu6JvcVZU'
 map_options = GMapOptions(lat=37.0902, lng=-95.7129, map_type="roadmap", zoom=3)
-#map_options = GMapOptions(lat=30.2861, lng=-97.7394, map_type="roadmap", zoom=11)
-map = gmap(googleAPI, map_options)
+    #map_options = GMapOptions(lat=30.2861, lng=-97.7394, map_type="roadmap", zoom=11)
 
-map.circle(
-    x="lon", 
-    y="lat", 
-    size=5, 
-    fill_color="blue", 
-    fill_alpha=0.8, 
+map_ = gmap(googleAPI, map_options)
+
+map_.circle(
+    x="lon",
+    y="lat",
+    size=5,
+    fill_color="blue",
+    fill_alpha=0.8,
     source=map_source,
-    
+
 )
 
 def update_slider(attr, old, new):
     year = new
     new_data = dict(
         x = data.loc[year].trainees_total,
-        y = data.loc[year].costs,    
+        y = data.loc[year].costs,
         location=data.loc[year].locationid,
     )
-    source.data = new_data
-    p.title.text = str(year)
+    source1.data = new_data
+    p1.title.text = str(year)
 
 def update_buttons(attr, old, new):
-    
-    print('test_list', radio_button_group.labels[new])
-    print('type', type(radio_button_group.labels[new]))
-    #print('dataframe ', data.head())
-    print('-------------')
+
+    # print('test_list', radio_button_group.labels[new])
+    # print('type', type(radio_button_group.labels[new]))
+    # print('dataframe ', data.head())
+    # print('-------------')
+    # print('selected column', selected_column)
+    # print('type selected_column', type(data[selected_column]))
 
     selected_column = str(radio_button_group.labels[new])
-    print('selected column', selected_column)
-    print('type selected_column', type(data[selected_column]))
     new_data = dict(
         x = data[selected_column],
         y = data.costs,
         location=data.locationid,
     )
-    source.data = new_data
-    p.xaxis.axis_label = str(selected_column)
+    source2.data = new_data
+    p2.xaxis.axis_label = str(selected_column)
 
 slider = Slider(start=2000, end=2016, value=2000, step=1, title="Year")
 slider.on_change('value', update_slider)
 
-radio_button_group = RadioButtonGroup(
-        labels=["trainees_total", "trainees_pass"], active=0)
+radio_button_group = RadioButtonGroup(labels=["trainees_total", "trainees_pass"], active=0)
 radio_button_group.on_change('active', update_buttons)
 
-layout = column(p, slider, widgetbox(radio_button_group), map)
+layout = column(p1, slider, p2, widgetbox(radio_button_group), map_)
 curdoc().add_root(layout)
 
 #bokeh serve filename.py --show
